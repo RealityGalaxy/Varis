d4eac04749716463ced960075633b133bc131f07
ovitoc <tocelisovidijus@gmail.com>
2024-02-20 21:30:59 +0200

    basic movement and double jump

--- Varis/scripts/Character.gd
+++ Varis/scripts/Character.gd
@@ -1,9 +1,41 @@
 extends CharacterBody2D
 
-var speed = 400
+var speed: int = 400
+var jump_speed: int = -1000
+var double_jump_speed: int = -700
+var jumps_left: int = 2
+var gravity = 2000
+var vel: Vector2 = Vector2.ZERO
+var accel: Vector2 = Vector2.ZERO
+
+func get_input():
+	vel.x = 0
+	if Input.is_action_pressed("right"):
+		vel.x += speed
+	if Input.is_action_pressed("left"):
+		vel.x -= speed
 
 func _physics_process(delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * speed
-
+	get_input()
+	
+	if is_on_floor() or is_on_ceiling():
+		jumps_left = 2 if is_on_floor() else 0
+		vel.y = 0
+		
+	vel.y += gravity * delta
+	
+	if Input.is_action_just_pressed("up"):
+		if is_on_floor():
+			jumps_left-=1
+			vel.y = jump_speed
+		elif jumps_left > 0:
+			jumps_left-=1
+			if vel.y < double_jump_speed:
+				vel.y -= abs(double_jump_speed) / (abs(vel.y)+1000) * abs(double_jump_speed)
+			elif vel.y < 0:
+				vel.y += double_jump_speed
+			else:
+				vel.y = double_jump_speed
+			
+	velocity = vel
 	move_and_slide()
