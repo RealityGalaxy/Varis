72976740a4bbc0746b5dd4eeac7c80bae14dccfa
Liffim <karolis.seimys@gmail.com>
2024-02-22 23:12:10 +0200

    Added Resource for storing info about stats

---
+++ Varis/scripts/StartingStats.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name StartingStats
+@export var elemental_name : String = "Element"
+@export var max_health : int = 100
+@export var max_mana : int = 50
+@export var strength : int = 10
+@export var defense : int = 5
+@export var speed : int = 20
+@export var cooldown : int = 1
