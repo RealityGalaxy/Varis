ac4145918ee3ef12785951a234ce26e4eae0ce19
Liffim <karolis.seimys@gmail.com>
2024-03-05 15:27:16 +0200

    Made stats manager
    moved scripts to one folder

--- Varis/scripts/StartingStats.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-
-class_name StartingStats
-@export var elemental_name : String = "Element"
-@export var max_health : int = 100
-@export var damage_multiplier : float = 1
-@export var damage_reduction : float = 1
-@export var speed_multiplier : float = 1
-@export var cooldown_reduction : float = 1
---
+++ Varis/scripts/StatsManager.gd
@@ -0,0 +1,32 @@
+extends Resource
+class_name StatsManager
+
+var max_health : int = 100
+var damage_multiplier : float = 1
+var damage_reduction : float = 1
+var speed_multiplier : float = 1
+var cooldown_reduction : float = 1
+
+func take_damage(damage):
+	# so that damage reduction would go from 1 to 2 :shrug:
+	var actual_damage = damage * (2 - damage_reduction) 
+	if actual_damage > 0:
+		max_health -= actual_damage
+		if max_health < 0:
+			max_health = 0
+			
+
+func increase_damage(amount):
+	damage_multiplier += amount
+	
+func increase_reduction(amount):
+	damage_reduction += amount
+
+func increase_speed(amount):
+	speed_multiplier += amount
+
+func increase_cooldown(amount):
+	cooldown_reduction += amount
+	
+func increase_health(amount):
+	max_health += amount
