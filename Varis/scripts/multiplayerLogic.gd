9dd36ab8f6b9e30518b61f3d1c3eda933ea0bfef
Shad0wb0t1337 <Edvinas.serapis@gmail.com>
2024-03-07 00:48:48 +0200

    try 2

--- Varis/scripts/multiplayerLogic.gd
+++ Varis/scripts/multiplayerLogic.gd
@@ -1,22 +1,36 @@
 extends Node2D
+
+var lobby_id = 0
+var peer = SteamMultiplayerPeer.new()
+
 @onready var multiplayerSpawner = $MultiplayerSpawner
 
-var peer = SteamMultiplayerPeer.new()
-var lobby_id = 0
-
-
 func _ready():
-	
+	multiplayerSpawner.spawn_function = spawn_level
+	peer.lobby_created.connect(_on_lobby_created)
 	Steam.lobby_match_list.connect(_on_lobby_match_list)
-	open_lobby_list()
-
+	
 func spawn_level(data):
 	var a = (load(data) as PackedScene).instantiate()
 	return a
-	
-func _on_back_pressed():
-	get_tree().change_scene_to_file("res://scenes/multiplayerScreen.tscn")
 
+
+func _on_host_pressed():
+	peer.create_lobby(SteamMultiplayerPeer.LOBBY_TYPE_PUBLIC)
+	multiplayer.multiplayer_peer = peer
+	multiplayerSpawner.spawn("res://scenes/level.tscn")
+	$Background.hide()
+	$MarginContainer.hide()
+	
+	
+	
+func _on_lobby_created(connect, id):
+	if connect:
+		lobby_id = id
+		Steam.setLobbyData(lobby_id, "name", str("godlobby"))
+		Steam.setLobbyJoinable(lobby_id, true)
+		print(lobby_id)
+		
 
 func join_lobby(id):
 	peer.connect_lobby(id)
@@ -24,12 +38,12 @@ func join_lobby(id):
 	lobby_id = id
 	$Background.hide()
 	$MarginContainer.hide()
+	$MarginContainer/HBoxContainer/LobbyScrollContainer/Lobbies.hide()
 	
-
 func open_lobby_list():
 	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_CLOSE)
 	Steam.requestLobbyList()
-	
+		
 func _on_lobby_match_list(lobbies):
 	
 	for lobby in lobbies:
@@ -45,6 +59,15 @@ func _on_lobby_match_list(lobbies):
 		#else:
 		#	var button = Button.new()
 		#	button.set_text("no lobbies :(")
-		#	button.set_size(Vector2(100, 5))		
+		#	button.set_size(Vector2(100, 5))
 		#	$MarginContainer/HBoxContainer/LobbyScrollContainer/Lobbies.add_child(button)
 		#	return
+
+
+func _on_join_pressed():
+	get_tree().change_scene_to_file("res://scenes/lobbiesScreen.tscn")
+	open_lobby_list()
+	#$MarginContainer.hide()
+	
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
