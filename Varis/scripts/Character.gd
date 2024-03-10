a481586782fc5b299b66b2244c875155bd8f6f2a
Alanas Švažas <60066251+MrEchox@users.noreply.github.com>
2024-03-10 19:14:23 +0200

    Vl 35 healthbar (#16)
    * No clue tbh
    
    Changed connections
    
    * VL-35. Updated healtbar and fix
    
    Added tween(k) for animation for damage bar. Fixed damage bar (signals). Removed B keybind for damage testing.
    
    Enjoy

--- Varis/scripts/Character.gd
+++ Varis/scripts/Character.gd
@@ -7,9 +7,14 @@ var jumps_left: int = 2
 var gravity = 2000
 var vel: Vector2 = Vector2.ZERO
 var stats : StatsManager
+@onready var healthbar = $Healthbar
 
 func _ready():
 	stats = StatsManager.new()
+	healthbar._init_healthbar(stats.current_health, stats.max_health)
+	stats.health_changed.connect(healthbar._set_health)
+	stats.max_health_changed.connect(healthbar._set_max_health)
+	
 
 func get_input():
 	vel.x = 0
@@ -17,7 +22,6 @@ func get_input():
 		vel.x += speed
 	if Input.is_action_pressed("left"):
 		vel.x -= speed
-
 
 func _physics_process(delta):
 	get_input()
