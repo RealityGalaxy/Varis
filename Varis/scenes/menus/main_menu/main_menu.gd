c0472b383773d3808a5aeb3d67b659396ded639a
Jolanta <baleviciutejolanta@gmail.com>
2024-03-19 16:13:36 +0200

    Settings

--- Varis/scenes/menus/main_menu/main_menu.gd
+++ Varis/scenes/menus/main_menu/main_menu.gd
@@ -7,7 +7,7 @@ func _on_multiplayer_pressed():
 	get_tree().change_scene_to_file("res://scenes/multiplayer/multiplayerScreen.tscn")
 
 func _on_settings_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/menus/settings/settings.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
