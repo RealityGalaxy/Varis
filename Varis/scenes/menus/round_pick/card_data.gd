fe5dbc3bf00c406960164e53905ac323a06d90e4
Alanas Švažas <asalanys@gmail.com>
2024-04-23 03:46:50 +0300

    :C

--- Varis/scenes/menus/round_pick/card_data.gd
+++ Varis/scenes/menus/round_pick/card_data.gd
@@ -5,12 +5,12 @@ var Cards = [
 	new_card("Rock throw", "Quickly throw many small rocks", "res://graphics/projectiles/basic_earth.png", "unlock_spell", "basic_earth"),
 	new_card("Water arrow", "Shoot a long thin arrow of water", "res://scenes/projectiles/basic_water/water_arrow.png", "unlock_spell", "basic_water"),
 	new_card("Air bolt", "Release bolts of air", "res://graphics/projectiles/air_bolt.png", "unlock_spell", "basic_air"),
-	new_card("Powerfull spells", "Increase damage by 20%", "res://graphics/projectiles/powerful_spell.png", "damage", 0.2),
-	new_card("Nimble feet", "Increase movement speed by 10%", "res://graphics/projectiles/nimble_feet.png", "speed", 0.1),
-	new_card("Tough skin", "Decrease damage taken by 10%", "res://graphics/projectiles/tough_skin.png", "reduction", 0.1),
-	new_card("Big brain", "Increase maximum mana by 30", "res://icon.svg", "max_mana", 30),
-	new_card("Bloody heart", "Increase maximum health by 30", "res://icon.svg", "max_health", 30),
-	new_card("Quickshot", "Decrease cooldowns by 15%", "res://icon.svg", "cdr", -0.15),
+	new_card("Powerfull spells", "Increase damage by 20%", "res://graphics/buffs/powerful_spell.png", "damage", 0.2),
+	new_card("Nimble feet", "Increase movement speed by 10%", "res://graphics/buffs/nimble_feet.png", "speed", 0.1),
+	new_card("Tough skin", "Decrease damage taken by 10%", "res://graphics/buffs/tough_skin.png", "reduction", 0.1),
+	new_card("Big brain", "Increase maximum mana by 30", "res://graphics/buffs/big_brain.png", "max_mana", 30),
+	new_card("Bloody heart", "Increase maximum health by 30", "res://graphics/buffs/bloody_heart.png", "max_health", 30),
+	new_card("Quickshot", "Decrease cooldowns by 15%", "res://graphics/buffs/quickshot.png", "cdr", -0.15),
 	new_card("Why?", "Decrease damage by 10%", "res://icon.svg", "damage", -0.1),
 ]
 
